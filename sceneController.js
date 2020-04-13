db68a4a1e09acde95c136bd912a344546f1243f1
MatthewDBarrett <12948612@student.uts.edu.au>
2020-04-14 00:07:58 +1000

    Seperated ship functionality
    Ship functionality has been added to the ship.js script

---
+++ sceneController.js
@@ -0,0 +1,112 @@
+import { Ship } from '../ship.js';
+
+var scene = new THREE.Scene();
+
+const clock = new THREE.Clock();
+
+var Pos = new THREE.Vector3(0,0,0);
+var Dir = new THREE.Vector3(0,0,0);
+
+//var isMouseDown = false;
+
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 10;
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var spaceShip = new Ship(Pos, Dir);
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = true;
+
+var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+keyLight.position.set(0, 0, 0);
+
+var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+fillLight.position.set(0, 0, 0);
+
+var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+backLight.position.set(0, 0, 0).normalize();
+
+// var camLight = new THREE.PointLight(new THREE.Color(1,1,1), 1000, 2000);
+// camLight.position.set(10,10,0);
+// camera.add(camLight);
+// scene.add(camera);
+
+var pointLight = new THREE.SpotLight(new THREE.Color(1,1,1), 1,100);
+pointLight.position.set(0,20,0);
+scene.add(pointLight);
+
+scene.add(keyLight);
+scene.add(fillLight);
+scene.add(backLight);
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( {color: 0x777777} );
+var cube = new THREE.Mesh( geometry, material );
+
+cube.position.set(0,-5,0);
+cube.scale.set(100,0.1,100);
+
+scene.add( cube );
+
+
+
+var animate = function () {
+	requestAnimationFrame( animate );
+  // if ( isMouseDown ) {
+	//    controls.update();
+  //  }
+	renderer.render(scene, camera);
+};
+
+export function AddToScene( object ) {
+  scene.add( object );
+}
+
+animate();
+// document.addEventListener( 'keydown', onKeyDown, false );
+// document.addEventListener( 'keyup', onKeyUp, false );
+// document.addEventListener( 'mousedown', onMouseDown, false );
+// document.addEventListener( 'mouseup', onMouseUp, false );
+
+// var canvas = renderer.domElement;
+// document.body.appendChild(canvas);
+//
+// var plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -10);
+// var raycaster = new THREE.Raycaster();
+// var mouse = new THREE.Vector2();
+// var pointOfIntersection = new THREE.Vector3();
+// canvas.addEventListener("mousemove", onMouseMove, false);
+//
+// var raycaster = new THREE.Raycaster();
+// var mouse = new THREE.Vector2();
+// var pointOfIntersection = new THREE.Vector3();
+// canvas.addEventListener("mousemove", onMouseMove, false);
+//
+// function onMouseMove(event){
+//   if (!isMouseDown) {
+//     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+//       mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+//     raycaster.setFromCamera(mouse, camera);
+//     raycaster.ray.intersectPlane(plane, pointOfIntersection);
+//     ship.lookAt(pointOfIntersection);
+//   }
+// }
+//
+// function onMouseDown(event){
+//   console.log("mouse down");
+//   controls.enabled = true;
+//
+//   isMouseDown = true;
+// }
+//
+// function onMouseUp(event){
+//   console.log("mouse up");
+//   isMouseDown = false;
+//   controls.enabled = false;
+// }
---
+++ ship.js
@@ -0,0 +1,210 @@
+import { AddToScene } from '../sceneController.js';
+
+const clock = new THREE.Clock();
+var delta = clock.getDelta();
+
+var Pos = new THREE.Vector3(0,0,0);
+var Dir = new THREE.Vector3(0,0,0);
+
+var ship = new THREE.Mesh();
+
+var increaseSpeed=false;
+var moveLeft=false;
+var decreaseSpeed=false;
+var moveRight=false;
+var rollLeft = false;
+var rollRight = false;
+
+var speed = 0;
+var minSpeed = 0;
+var maxSpeed = 10;
+
+var yaw = 0;
+var minYaw = 0;
+var maxYaw = 0;
+
+var pitch = 0;
+var minPitch = 0;
+var maxPitch = 0;
+
+export function Ship(position, direction){
+  this.Pos = position;
+  this.Dir = direction;
+  init();
+}
+
+function init(){
+  CreateShip(
+    '/textures/',     //texture file path
+    'viper2.mtl',     //texture file name
+    '/models/',       //model file path
+    'viper2.obj'      //model file name
+  );
+}
+
+function CreateShip(texturePath, textureFile, modelPath, modelFile){
+  var mtlLoader = new THREE.MTLLoader();
+  mtlLoader.setTexturePath( texturePath );
+  mtlLoader.setPath( modelPath );
+  mtlLoader.load( textureFile , function (materials) {    //load textures
+
+  materials.preload();
+
+  var objLoader = new THREE.OBJLoader();
+  objLoader.setMaterials(materials);        //assign textures
+  objLoader.load( modelPath.concat( modelFile ) , function (object) {
+
+      ship = object;
+      ship.rotation.set(0,4,0);
+      AddToScene( ship );
+    });
+  });
+}
+
+// export function GetShip(){
+//   return ship;
+// }
+
+function animate(){
+  delta = clock.getDelta();
+
+  requestAnimationFrame( animate );
+  ShipControls();
+
+  if ( speed > 0 )
+    ship.translateZ( speed * delta );
+}
+
+function ShipControls(){
+  var delta = clock.getDelta();
+
+  if (moveLeft) {
+
+  }
+  if (moveRight) {
+    
+  }
+  if (increaseSpeed) {
+    Pos.z += speed * delta;
+  }
+  if (decreaseSpeed) {
+    Pos.z -= speed * delta;
+  }
+  if (rollLeft){
+    Dir.z -= speed * delta;
+  }
+  if (rollRight){
+    Dir.z += speed * delta;
+  }
+
+  ship.position.set(Pos.x,Pos.y,Pos.z);
+  ship.rotation.set(Dir.x,Dir.y,Dir.z);
+
+  // if (isMouseDown)
+  //   controls.target = new THREE.Vector3(Pos.x,Pos.y,Pos.z);
+
+  //camera.position.set(Pos.x+10,Pos.y+10,Pos.z+10);
+  //camera.lookAt(ship);
+
+}
+
+var onKeyDown = function ( event ) {
+
+  switch ( event.keyCode ) {
+
+    case 87: // w
+      increaseSpeed = true;
+      break;
+
+    case 65: // a
+      moveLeft = true;
+      break;
+
+    case 83: // s
+      decreaseSpeed = true;
+      break;
+
+    case 68: // d
+      moveRight = true;
+      break;
+    case 81: // q
+      rollLeft = true;
+      break;
+    case 69: // e
+      rollRight = true;
+      break;
+  }
+
+};
+
+var onKeyUp = function ( event ) {
+
+  switch( event.keyCode ) {
+
+    case 87: // w
+      increaseSpeed = false;
+      break;
+
+    case 65: // a
+      moveLeft = false;
+      break;
+
+    case 83: // s
+      decreaseSpeed = false;
+      break;
+
+    case 68: // d
+      moveRight = false;
+      break;
+    case 81: // q
+      rollLeft = false;
+      break;
+    case 69: // e
+      rollRight = false;
+      break;
+  }
+};
+
+document.addEventListener( 'keydown', onKeyDown, false );
+document.addEventListener( 'keyup', onKeyUp, false );
+// document.addEventListener( 'mousedown', onMouseDown, false );
+// document.addEventListener( 'mouseup', onMouseUp, false );
+
+// var canvas = renderer.domElement;
+// document.body.appendChild(canvas);
+//
+// var plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -10);
+// var raycaster = new THREE.Raycaster();
+// var mouse = new THREE.Vector2();
+// var pointOfIntersection = new THREE.Vector3();
+// canvas.addEventListener("mousemove", onMouseMove, false);
+//
+// var raycaster = new THREE.Raycaster();
+// var mouse = new THREE.Vector2();
+// var pointOfIntersection = new THREE.Vector3();
+// canvas.addEventListener("mousemove", onMouseMove, false);
+//
+// function onMouseMove(event){
+//   if (!isMouseDown) {
+//     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+//       mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+//     raycaster.setFromCamera(mouse, camera);
+//     raycaster.ray.intersectPlane(plane, pointOfIntersection);
+//     ship.lookAt(pointOfIntersection);
+//   }
+// }
+//
+// function onMouseDown(event){
+//   console.log("mouse down");
+//   controls.enabled = true;
+//
+//   isMouseDown = true;
+// }
+//
+// function onMouseUp(event){
+//   console.log("mouse up");
+//   isMouseDown = false;
+//   controls.enabled = false;
+// }
+
+animate();
