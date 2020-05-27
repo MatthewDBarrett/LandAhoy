5b6775361e29505f45033540755515d0226e7baa
Ethan Goh <ethangoh98@gmail.com>
2020-05-28 02:55:04 +1000

    Finished basic generation of particles

--- particleGeneration.js
+++ particleGeneration.js
@@ -1,28 +1,96 @@
 export class ParticleGen {
-    
-    constructor(pos, dir, scene) {
+    constructor(pos, dir, maxParticles, maxLifetime, maxSpeed, scene, autoGen) {
         this.pos = pos;
         this.dir = dir;
         this.scene = scene;
         this.particles = [];
+        this.maxParticles = maxParticles;
+        this.maxLifetime = maxLifetime;
+        this.autoGen = autoGen;
+        this.maxSpeed = maxSpeed;
+        this.clock = new THREE.Clock();
+        this.delta = this.clock.getDelta();
+    }
+
+    autoLoop(){
+        this.delta = this.clock.getDelta();
+        if(this.autoGen){
+            if(this.particles.length < this.maxParticles){
+                this.generateParticle();
+            }
+        }
+        if(this.particles.length > 0){   
+            for(var i = 0; i < this.particles.length; i++){
+                this.updateParticle(this.particles[i], this.delta);
+                this.lifetimeDec(this.particles[i], this.delta);   
+                this.checkLifetime(this.particles[i], i);
+                this.updateOpacity(this.particles[i]);
+            }
+        }
     }
 
     generateParticle(){
-        var particle = new Particle(new THREE.Vector3(0, 0, 0), new THREE.Vector3(1, 0, 0), new THREE.Vector3(0, 0, 1), 111, 0.0001);
+        //Generate particle
+        var particle = new Particle(
+            this.pos, 
+            new THREE.Vector3(1, 0, 0), 
+            this.dir, 
+            Math.floor(Math.random() * this.maxLifetime) + 1,
+            Math.floor(Math.random() * this.maxSpeed) + 1);
+
+        this.particles.push(particle);
+        this.addToScene(particle);
     }
 
     addToScene(particle){
-        this.scene.add(particle);
+        this.scene.add(particle.getParticleMesh());
     }
-    //TODO
-    /*
-    1) Spawn a particle(add it at the end)
-    2) Delete a particle(delete a particle at a given index)
-    3) Check Lifetime of particle
-    4) Destroy all particles
-    5) Set spawn rate
-    6) 
-    */
+
+    lifetimeDec(particle, delta){
+        particle.lifetimeDecrement(delta);
+    }
+
+    updateParticle(particle, delta){
+        particle.updateParticle(delta);
+    }
+
+    checkLifetime(particle, index){
+        if(particle.getLifetime() < 0){
+            this.removeParticle(particle, index);
+        }
+    }
+
+    updateOpacity(particle){
+        particle.setOpacity();
+    }
+
+    removeParticle(particle, index){
+        var particle = this.particles[index];
+        this.particles.splice(index, 1);
+        this.deleteParticleFromScene(particle);
+    }
+
+    deleteParticleFromScene(particle){
+        particle.particleMesh.geometry.dispose();
+        particle.particleMesh.material.dispose();
+        this.scene.remove(particle.getParticleMesh());
+        particle.particleMesh = null;
+    }
+
+    destroyAll(){
+        for(var i = 0; i < this.particles.length; i++){
+            this.deleteParticleFromScene(particle);
+        }
+        this.particles = [];
+    }
+
+    setAutoGen(){
+        this.autoGen = !this.autoGen;
+    }
+
+    setPos(Pos){
+        this.pos = Pos;
+    }
 }
 
 //Export for testing purposes!
@@ -33,9 +101,11 @@ export class Particle{
         this.rot = rot;
         this.dir = dir;
         this.lifetime = lifetime;
+        this.initialLifetime = lifetime;
+        this.initSpeed = initSpeed;
         this.speed = initSpeed;
         this.geometry = new THREE.Geometry();
-        this.material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+        this.material = new THREE.MeshNormalMaterial( );// {color: 0x00ff00}
         this.particleMesh = null;
         this.direction = dir;
         this.cubeGeometry = new THREE.BoxGeometry(0.1,0.1,0.1);
@@ -53,6 +123,7 @@ export class Particle{
         this.geometry.faces.push( new THREE.Face3(0, 1, 2) );   
         this.geometry.computeFaceNormals();
         this.material.side = THREE.DoubleSide;
+        this.material.transparent = true;
         this.material.wireframe = false;
         this.geometry.translate( -0.5, -0.5, 0 );
 
@@ -74,7 +145,7 @@ export class Particle{
 
     //decrease lifetime
     lifetimeDecrement(dec){
-        lifetime -= dec;
+        this.lifetime -= dec;
     }
 
     getParticleMesh(){
@@ -85,6 +156,19 @@ export class Particle{
         return this.cube;
     }
 
+    setOpacity(){
+        if(this.lifetime/this.initialLifetime < 0){
+            this.particleMesh.material.opacity = 0;
+        }
+        else{
+            this.particleMesh.material.opacity = this.lifetime/this.initialLifetime;
+        }
+    }
+
+    setCurrentSpeed(){
+        speed = (this.lifetime/this.initialLifetime) * 100;
+    }
+
     //update position
     updateParticle(delta){
         this.rot.x += 0.1;
@@ -93,9 +177,8 @@ export class Particle{
         if(this.speed > 0){            
             this.cube.getWorldDirection(this.direction);
             this.cube.position.add( this.direction.multiplyScalar(this.speed));
-            //this.particleMesh.rotation.set(this.rot.x, this.rot.y, this.rot.z);
+            this.particleMesh.rotation.set(this.rot.x, this.rot.y, this.rot.z);
             this.particleMesh.position.set(this.cube.position.x, this.cube.position.y, this.cube.position.z);
-            console.log(this.cube.position);
         }
     }
 }
